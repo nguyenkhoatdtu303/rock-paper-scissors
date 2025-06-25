a1f37e8f9ce805167d6847440bbea5a3f9783ebb
KEVIN-NGUYEN <nguyenkhoatdtu303@gmail.com>
2025-06-25 12:31:18 +0700

    Add function

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+let computerScore = 0;
+let humanScore = 0;
 
 function getComputerChoice() {
     let choice = Math.floor(Math.random() * 3);
@@ -11,7 +13,42 @@ function getComputerChoice() {
 
 function getHumanChoice() {
     let choice = prompt("Your choice: ");
-    return choice;
+    return choice.toLowerCase();
 }
 
-console.log(getHumanChoice());
+function playRound(humanChoice, computerChoice) {
+    if (humanChoice === "rock") {
+        if (computerChoice === "scissors") {
+            console.log("You win! rock beats scissors")
+            humanScore += 1;
+        } else if (computerChoice === "paper") {
+            console.log("You lose! paper beats rock")
+            computerScore += 1;
+        } else {
+            console.log("Draws!");
+        }
+    } else if (humanChoice === "paper") {
+        if (computerChoice === "rock") {
+            console.log("You win! paper beats rock")
+            humanScore += 1;
+        } else if (computerChoice === "scissors") {
+            console.log("You lose! scissors beats paper")
+            computerScore += 1;
+        } else {
+            console.log("Draws!");
+        } 
+    } else if (humanChoice === "scissors") {
+        if (computerChoice === "papper") {
+            console.log("You win! scissors beats paper")
+            humanScore += 1;
+        } else if (computerChoice === "rock") {
+            console.log("You lose! rock beats scissors")
+            computerScore += 1;
+        } else {
+            console.log("Draws!");
+        }
+    }
+} 
+
+playRound(getHumanChoice(), getComputerChoice());
+console.log(humanScore, computerScore);
